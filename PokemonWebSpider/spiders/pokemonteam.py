f5979edea58c88afca4db41ff7a1fcb7dfc59167
Joaquim Codina <joaquim.codina@e-campus.uab.cat>
2019-11-10 21:29:42 +0100

    Select six random pokemon and show them per screen

--- PokemonWebSpider/spiders/pokemonteam.py
+++ PokemonWebSpider/spiders/pokemonteam.py
@@ -15,3 +15,34 @@ class PokemonTeam(scrapy.Spider):
         name = response.css(parser.get('response', 'name')).extract()
         lenType = response.css(parser.get('response', 'type')).extract()
         sprite = response.css(parser.get('response', 'sprite')).extract()
+
+        def check(value, list):
+            valid = 0
+            if value not in list:
+                valid = 1
+                return valid
+            else:
+                return valid
+
+        items = []
+        namePokemons = []
+        typesList = []
+        for i in range(6):
+            nombre = random.choice(name)
+
+            typesLen = lenType[name.index(nombre)].split(" · ")
+            currentType = lenType[name.index(nombre)]
+            validType = check(currentType, typesList)
+
+            while nombre in map(lambda x: x, namePokemons) or len(typesLen) == 1 or validType == 0:
+                nombre = random.choice(name)
+
+                typesLen = lenType[name.index(nombre)].split(" · ")
+                currentType = lenType[name.index(nombre)]
+                validType = check(currentType, typesList)
+
+            namePokemons.append(nombre)
+            typesList.append(currentType)
+
+        print("My Team")
+        print(namePokemons)
